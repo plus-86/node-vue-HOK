fb6e82f500f914028a286d1db9fa7668bdf7376d
a3a4aa5 <linlgd@outlook.com>
2022-11-06 15:49:53 +0800

    edit Hero Model

--- server/models/Hero.js
+++ server/models/Hero.js
@@ -2,7 +2,43 @@ const mongoose = require('mongoose')
 
 const heroSchema = new mongoose.Schema({
     name: { type: String },
-    avatar: { type: String }
+    avatar: { type: String },
+    title: { type: String },
+    // 英雄分类关联到Category表,一个英雄可能属于多个分类所以是数组
+    Categories: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Category' }],
+    // 评分
+    scores: {
+        difficult: { type: Number },
+        skills: { type: Number },
+        attack: { type: Number },
+        survive: { type: Number },
+    },
+    // 技能
+    skills: [{
+        icon: { type: String },
+        name: { type: String },
+        description: { type: String },
+        tips: { type: String },
+    }],
+    // 顺风/逆风出装
+    items1: [{
+        type: mongoose.SchemaTypes.ObjectId, ref: 'Item'
+    }],
+    items2: [{
+        type: mongoose.SchemaTypes.ObjectId, ref: 'Item'
+    }],
+    // 使用技巧
+    usageTips: { Type: String },
+    // 对抗技巧
+    battleTips: { Type: String },
+    // 团战思路
+    teamTips: { Type: String },
+    // 最佳搭档
+    partners: [{
+        hero: { type: mongoose.SchemaTypes.ObjectId, ref: 'Hero' },
+        description: { type: String }
+    }]
+
 })
 
 module.exports = mongoose.model('Hero', heroSchema)
