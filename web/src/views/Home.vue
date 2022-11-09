84e933ed1c54737229f69480801cf6aefafa3f73
a3a4aa5 <linlgd@outlook.com>
2022-11-09 13:51:56 +0800

    sprite

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -15,11 +15,30 @@
           alt=""
         />
       </swiper-slide>
+      <swiper-slide>
+        <img
+          class="w-100"
+          src="@/assets/images/468f663b9e4290b435061eb877595a66.jpeg"
+          alt=""
+        />
+      </swiper-slide>
       <div
         class="swiper-pagination pagination-home text-right px-3 pb-2"
         slot="pagination"
       ></div>
     </swiper>
+    <div class="nav-icons bg-white mt-3 text-center pt-3 text-dark-1">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3" v-for="n in 10" :key="n">
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+      </div>
+      <div class="bg-light py-2 fs-sm">
+        <i class="sprite sprite-arrow mr-1"></i>
+        <span>收起</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,10 +49,17 @@ export default {
       swiperOptions: {
         pagination: {
           el: '.pagination-home'
-        }
+        },
         // Some Swiper option/callback...
+        autoplay: {
+          delay: 3000
+        },
+        loop: true
       }
     }
+  },
+  mounted() {
+    console.log()
   }
 }
 </script>
@@ -46,6 +72,18 @@ export default {
     background: map-get($colors, 'white');
     &.active {
       background: map-get($colors, 'info');
+    }
+  }
+}
+
+.nav-icons {
+  border-top: 1px solid $border-color;
+  border-bottom: 1px solid $border-color;
+  .nav-item {
+    width: 25%;
+    border-right: 1px solid $border-color;
+    &:nth-child(4n) {
+      border-right: none;
     }
   }
 }
