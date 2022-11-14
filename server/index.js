e9b6e7734fa95050c952b39b73e59ab129dfbee7
a3a4aa5 <linlgd@outlook.com>
2022-11-15 00:18:55 +0800

    cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
-// 跨域包
-const cors = require('cors')()
+
+
 
 const express = require('express')
 
 const app = express()
+// 跨域包 写在路由前
+app.use(require('cors')())
 // 使用express的static中间件,表示用户访问http://localhost:3000/uploads时,可以获取到项目uploads文件内所有静态资源
 app.use('/uploads', express.static(__dirname + '/uploads'))
 app.use('/admin', express.static(__dirname + '/admin'))
